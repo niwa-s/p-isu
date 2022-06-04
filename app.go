2735109bb7b6d84c8e475d0b8c5d697e6a42a705
niwa-s <souta.niwa.prog@gmail.com>
2022-06-05 02:12:37 +0900

    a

--- app.go
+++ app.go
@@ -727,11 +727,12 @@ func postIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := "INSERT INTO `posts` (`user_id`, `mime`, `body`) VALUES (?,?,?)"
+	query := "INSERT INTO `posts` (`user_id`, `mime`, `imgdata`, `body`) VALUES (?,?,?,?)"
 	result, err := db.Exec(
 		query,
 		me.ID,
 		mime,
+		[]byte(""),
 		r.FormValue("body"),
 	)
 	if err != nil {
